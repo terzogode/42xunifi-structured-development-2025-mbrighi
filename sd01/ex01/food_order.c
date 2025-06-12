8374f0e689dd3e9bea376a95eb891aa0870bc069
Matteo <mbrighi@student.42firenze.it>
2025-06-12 23:58:29 +0200

    da migliorare

--- sd01/ex01/food_order.c
+++ sd01/ex01/food_order.c
@@ -6,7 +6,7 @@
 /*   By: mbrighi <mbrighi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 19:13:24 by mbrighi           #+#    #+#             */
-/*   Updated: 2025/06/12 19:51:11 by mbrighi          ###   ########.fr       */
+/*   Updated: 2025/06/12 23:37:22 by mbrighi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,16 @@ int	process_food_order(struct OrderRequest *request)
 	struct OrderConfirmation	*ord_conf;
 
 	if (check_restaurant_status(request) == 1)
-	{
 		ord_conf = create_standard_confirmation();
-		if (ord_conf == NULL)
-			return (0);
-		send_confirmation_notification(ord_conf);
-	}
 	if (check_restaurant_status(request) == 0)
-	{
 		ord_conf = create_preorder_confirmation();
-		if (ord_conf == NULL)
-			return (0);
-		send_confirmation_notification(ord_conf);
+	if (ord_conf == NULL)
+	{
+		free(request);
+		return (0);
 	}
+	send_confirmation_notification(ord_conf);
+	free(request);
+	free(request);
+	return (1);
 }
